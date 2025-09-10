066b05852a4006f5d48f9a4e9ed995e9bd5ae5bf
Shukran Badshah <shukranbadshah88@gmail.com>
2025-09-10 17:20:47 +0500

    errors fixed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
- import User from "../models/user.model";
+ import User from "../models/user.model.js";
  import bcrypt from "bcrypt";
  import jwt from "jsonwebtoken"
 
@@ -18,14 +18,17 @@ newUser.password= await bcrypt.hash(password,10)
 await newUser.save()
 
 //jwt
+const jwttokin= jwt.sign({email:newUser.email , _id:newUser._id} , process.env.JWT_SECERT,
+        {expiresIn:"20h"})
 res.cookie("token",jwttokin)
+
 res.status(200).json({
   message:"Login succesfully", 
   success:true,
   user:{
-  email:exist.email, //the new user is store in exist so thats why we used exist.
-  name:exist.name,
-  _id:exist._id
+  email:newUser.email, //the new user is store in exist so thats why we used exist.
+  name:newUser.name,
+  _id:newUser._id
   }
 })
 
@@ -48,7 +51,7 @@ return res.status(400).json({
   message:"Authentication fail ", success: false
 })
 }
-const passverify= bcrypt.compare(password,User.password)
+const passverify= await bcrypt.compare(password,exist.password)
 if(!passverify){
   return res.status(400).json({
   message:"Authentication fail ", success: false
@@ -56,7 +59,7 @@ if(!passverify){
       }
 
 //jwt
-const jwttokin= jwt.sign({email:User.email , _id:User._id} , process.env.JWT_SECERT,
+const jwttokin= jwt.sign({email:exist.email , _id:exist._id} , process.env.JWT_SECERT,
         {expiresIn:"20h"}
       )
   res.cookie("token",jwttokin)
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Register,login} from "../controllers/auth.controller";
+import { Register,login} from "../controllers/auth.controller.js";
 
 const route=express.Router();
 
